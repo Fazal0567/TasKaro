d6bece6b8f1e0a787cada34bade1f19fe6d394ee
Mohd Fazal Ali <fazalalicontribute@gmail.com>
2025-07-10 00:57:59 +0530

    Update Login.jsx

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -14,7 +14,7 @@ const Login = ({ onSubmit, onSwitchMode }) => {
 	const [formData, setFormData] = useState(INITIAL_FORM);
 	const [rememberMe, setRememberMe] = useState(false);
 	const navigate = useNavigate();
-	const url = "http://localhost:4000";
+	const url = "https://taskaro-ybh1.onrender.com";
 
 	useEffect(() => {
 		const token = localStorage.getItem("token");
